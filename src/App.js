ca595875ee56dbef860c736cf55fe56940b06264
Chandra Meher <chamehe1@in.ibm.com>
2020-06-27 19:16:03 +0530

    Modified Dashboard and App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
-import DashBoard from './Components/DashBoard';
+import DashBoard from './Components/DashBoard'
 
 function App() {
   return (
     <div className="App">
       <h1>Hello Assignment</h1>
-      <DashBoard />
+        <DashBoard />
+
     </div>  
   );
 }
--- src/Components/DashBoard.js
+++ src/Components/DashBoard.js
@@ -1,72 +1,13 @@
-import React, { Component } from 'react'
-import  axios  from "axios";
+import React from 'react'
+import MarginAndFluctuation from './Recipes/MarginAndFluctuation/MarginAndFluctuation'
+import RecipeTabs from './Recipes/DisplayRecipes/RecipeTab'
 
-import RecipeTabs from './Recipes/DisplayRecipes/RecipeTab';
-import MarginAndFluctuation from './Recipes/MarginAndFluctuation/MarginAndFluctuation';
-
-export default class DashBoard extends Component {
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-             pageNumber:1
-        }
-    }
-    
-    componentWillMount(){
-        let url = 'https://beta.eagleowl.in/api/v1/mock/organization/18/outlet/18/recipe/recipes/'
-        axios.get(url,{
-            params:{
-                page: this.state.pageNumber
-            }
-        })
-        .then(res=>console.log(res.data))
-        .catch(err=>console.log(err))
-    }
-     
-    componentDidMount() {
-        // let url = 'https://beta.eagleowl.in/api/v1/mock/organization/18/outlet/18/recipe/recipes/'
-        // axios.get(url,{
-        //     params:{
-        //         page: 1
-        //     }
-        // })
-        // .then(res=>console.log(res.data))
-        // .catch(err=>console.log(err))
-      //  this.changePage();
-    }
-
-    changePage(){
-        let url = 'https://beta.eagleowl.in/api/v1/mock/organization/18/outlet/18/recipe/recipes/'
-        axios.get(url,{
-            params:{
-                page: this.state.pageNumber
-            }
-        })
-        .then(res=>console.log(res.data))
-        .catch(err=>console.log(err))
-    }
-
-    handlePage=()=>{
-        console.log("button pressed!!");
-        this.setState({
-            pageNumber:this.state.pageNumber+1
-        },()=>this.changePage())
-
-        
-    }
-
-    render() {
-        return (
+export default function Display() {
+    return (
+        <div>
+            <MarginAndFluctuation />
+            <RecipeTabs />
             
-            <div>
-                <>
-                <MarginAndFluctuation />
-                 </>
-                {/* hello dear
-                <button onClick={()=>this.handlePage()}>ChangePage</button> */}
-                <RecipeTabs />
-            </div>
-        )
-    }
+        </div>
+    )
 }
